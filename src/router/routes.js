2070393f3e79ad4b99d24c28a03984dd07b4ab98
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-10-02 01:44:23 -0300

    updating routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,28 +1,11 @@
 import Login from '../views/Login/Login';
 import PrincipalDashboard from '../views/Dashboard/PrincipalDashboard';
-import StudentDashboard from '../views/Dashboard/StudentDashboard';
-import ClassroomDashboard from '../views/Dashboard/ClassroomDashboard';
 import ForgotPassword from '../views/ForgotPassword/ForgotPassword';
-import Importation from '../views/Importation/Importation';
-import ImportActivity from '../views/Importation/ImportActivity';
-import ImportAttendance from '../views/Importation/ImportAttendance';
-import ExportActivity from '../views/Exportation/ExportActivity';
-import ExportAttendance from '../views/Exportation/ExportAttendance';
-import MyProfile from '../views/Profile/MyProfile';
+import CollaboratorDetail from '../views/Collaborator/CollaboratorDetail';
 
 export const routes = [
     { path: '/', component: Login, name: 'login' },
     { path: '/home', component: PrincipalDashboard, name: 'home' },
     { path: '/forgot_password', component: ForgotPassword, name: 'forgot_password' },
-    { path: '/collaborator', component: ForgotPassword, name: 'forgot_password' },
-
-    { path: '/analysis/student', component: StudentDashboard, name: 'student' },
-    { path: '/analysis/classroom', component: ClassroomDashboard, name: 'classroom' },
-
-    { path: '/import', component: Importation, name: 'import' },
-    { path: '/import/activity', component: ImportActivity, name: 'import_activity' },
-    { path: '/import/attendance', component: ImportAttendance, name: 'import_attendance' },
-    { path: '/export/activity', component: ExportActivity, name: 'export_activity' },
-    { path: '/export/attendance', component: ExportAttendance, name: 'export_attendance' },
-    { path: '/profile', component: MyProfile, name: 'profile' }
+    { path: '/collaborator/analysis', component: CollaboratorDetail, name: 'collaborator_detail' },
 ];
