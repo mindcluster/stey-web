0003a3afd3cf5d197d09161054a951e9033b5ea6
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-10-08 22:26:40 -0300

    including method

--- src/mixins/globalMethods.vue
+++ src/mixins/globalMethods.vue
@@ -17,6 +17,23 @@ export default {
         return moment(date).format("DD/MM/YYYY");
       }
     },
+    diffBetweenDates(pastDate) {
+      const currentDate = moment();
+
+      const now = currentDate.format("DD/MM/YYYY");
+      const past = this.formatDate(pastDate);
+
+      var diff = moment(now,"DD/MM/YYYY").diff(moment(past,"DD/MM/YYYY"));
+
+      var months = moment.duration(diff).asMonths().toFixed(0);
+
+      if(months > 0){
+        return months + 'm';
+      }
+      else {
+        return '-'
+      }
+    },
   },
 };
 </script>
