fce7719f34ca45d5efad8030e21d951816c3f153
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-10-14 20:51:11 -0300

    updating components

--- src/components/lists/ScrollPromotions.vue
+++ src/components/lists/ScrollPromotions.vue
@@ -1,58 +1,27 @@
 <template>
   <v-card class="scroll-list" flat>
-    <h4 class="list-title">Indicador De Promoção</h4>
-    <v-list class="content" dense>
-      <v-list-item
-        class="indicator"
-        @click.native="setCollaborator(item)"
-        v-for="(item, i) in params"
-        :key="i"
-        style="color: var(--blackStey) !important"
-      >
-        <v-list-item-content>
-          <v-icon x-large :color="returnColor(item.status)">
-            {{ returnIcon(item.status) }}
-          </v-icon>
-          <v-list-item-title>{{ item.name }}</v-list-item-title>
-          <v-list-item-title>{{ item.job_role }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
+    <h4 class="list-title">Indicados Para Promoção</h4>
+    <div class="content" dense>
+      <div class="item">
+        <v-avatar
+          color="teal"
+          size="56"
+        >
+        90
+        </v-avatar>
+        <h6>Nome</h6>
+        <h6>Sl</h6>
+      </div>
+    </div>
   </v-card>
 </template>
 
 <script>
 import globalMethods from "../../mixins/globalMethods";
-import { mapMutations } from "vuex";
-
 export default {
   name: "ScrollPromotions",
   mixins: [globalMethods],
-  props: ["params"],
-  methods: {
-    ...mapMutations(["SET_COLLABORATOR"]),
-    setCollaborator(item) {
-      this.SET_COLLABORATOR(item);
-      localStorage.setItem("selected_collaborator", JSON.stringify(item));
-      this.$router.push({ name: "collaborator_detail" });
-    },
-    returnColor(status) {
-      switch (status) {
-        case "Promotion":
-          return "var(--greenAlert)";
-        default:
-          return "var(--orangeAlert)";
-      }
-    },
-    returnIcon(status) {
-      switch (status) {
-        case "Promotion":
-          return "mdi-check-circle";
-        default:
-          return "mdi-close-circle";
-      }
-    },
-  },
+  props: ["number", "text", "color", "params"],
 };
 </script>
 
@@ -85,14 +54,6 @@ export default {
   text-align: center;
 }
 
-v-list-item-content {
-  text-align: center;
-}
-
-.indicator {
-  cursor: pointer;
-}
-
 @media only screen and (max-width: 1024px) {
   .scroll-list {
     font-family: "Metropolis Regular";
@@ -102,4 +63,4 @@ v-list-item-content {
     margin-bottom: 2em;
   }
 }
-</style>]
+</style>
