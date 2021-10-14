9452586fc99487917b4360345409034afcf71e9d
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-10-14 20:50:29 -0300

    updating components

--- src/components/lists/ScrollPromotions.vue
+++ src/components/lists/ScrollPromotions.vue
@@ -3,14 +3,15 @@
     <h4 class="list-title">Indicador De Promoção</h4>
     <v-list class="content" dense>
       <v-list-item
+        class="indicator"
+        @click.native="setCollaborator(item)"
         v-for="(item, i) in params"
         :key="i"
-        :disabled="true"
         style="color: var(--blackStey) !important"
       >
         <v-list-item-content>
           <v-icon x-large :color="returnColor(item.status)">
-            {{ returnIcon(item.status) }}  
+            {{ returnIcon(item.status) }}
           </v-icon>
           <v-list-item-title>{{ item.name }}</v-list-item-title>
           <v-list-item-title>{{ item.job_role }}</v-list-item-title>
@@ -22,11 +23,19 @@
 
 <script>
 import globalMethods from "../../mixins/globalMethods";
+import { mapMutations } from "vuex";
+
 export default {
   name: "ScrollPromotions",
   mixins: [globalMethods],
   props: ["params"],
   methods: {
+    ...mapMutations(["SET_COLLABORATOR"]),
+    setCollaborator(item) {
+      this.SET_COLLABORATOR(item);
+      localStorage.setItem("selected_collaborator", JSON.stringify(item));
+      this.$router.push({ name: "collaborator_detail" });
+    },
     returnColor(status) {
       switch (status) {
         case "Promotion":
@@ -43,7 +52,7 @@ export default {
           return "mdi-close-circle";
       }
     },
-  }
+  },
 };
 </script>
 
@@ -79,6 +88,11 @@ export default {
 v-list-item-content {
   text-align: center;
 }
+
+.indicator {
+  cursor: pointer;
+}
+
 @media only screen and (max-width: 1024px) {
   .scroll-list {
     font-family: "Metropolis Regular";
